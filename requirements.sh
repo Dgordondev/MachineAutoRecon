61c0d2f017a0ce75debcef0ce6e37e44eaa390a8
Dgordondev <91124845+Dgordondev@users.noreply.github.com>
2024-05-28 12:54:47 +0200

    Add xclip to requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-
 install_debian() {
-    echo "Instalando Nmap y WhatWeb en sistemas basados en Debian..."
+    echo "Instalando requisitos en sistemas basados en Debian..."
     sudo apt update
-    sudo apt install -y nmap whatweb
+    sudo apt install -y nmap whatweb xclip
 }
 
 install_redhat() {
-    echo "Instalando Nmap y WhatWeb en sistemas basados en Red Hat..."
-    sudo yum install -y nmap whatweb
+    echo "Instalando requisitos en sistemas basados en Red Hat..."
+    sudo yum install -y nmap whatweb xclip
 }
 
 if [ -x "$(command -v apt)" ]; then
@@ -17,12 +16,12 @@ if [ -x "$(command -v apt)" ]; then
 elif [ -x "$(command -v yum)" ]; then
     install_redhat
 else
-    echo "No se pudo detectar el gestor de paquetes compatible. Por favor, instala Nmap y WhatWeb manualmente."
+    echo "No se pudo detectar el gestor de paquetes compatible. Por favor, instala Nmap, WhatWeb y Xclip manualmente."
     exit 1
 fi
 
 if [ -x "$(command -v nmap)" ] && [ -x "$(command -v whatweb)" ]; then
-    echo "Nmap y WhatWeb se instalaron correctamente."
+    echo "Los requisitos se instalaron correctamente."
 else
-    echo "¡Hubo un problema durante la instalación de Nmap y/o WhatWeb!"
+    echo "¡Hubo un problema durante la instalación de los requisitos!"
 fi
